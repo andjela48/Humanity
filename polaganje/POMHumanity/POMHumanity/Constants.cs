2caaf37496049ce5a0994f46e69eca6b19941cea
andjela48 <57489312+andjela48@users.noreply.github.com>
2020-02-07 13:32:28 +0100

    Add files via upload

---
+++ polaganje/POMHumanity/POMHumanity/Constants.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace POMHumanity
+{
+    public class Constants
+    {
+        public static readonly string WBDRIVER_PATH = @"C:\Users\Korisnik\Desktop\chrome driver\chromedriver_win32";
+        public static readonly string Excel_Path = @"C:\Users\Korisnik\Desktop\data.xlsx";
+        public static readonly string Zaposleni_Path = @"C:\Users\Korisnik\Desktop\Zaposleni.xlsx";
+       
+    }
+}
---
+++ polaganje/POMHumanity/POMHumanity/ExcelUtility.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IronXL;
+
+
+namespace POMHumanity
+{
+    public class ExcelUtility
+    {
+        private static WorkBook wb = null;
+        private static WorkSheet ws = null;
+        public static bool OpenFile(string zaposleni)
+        {
+            try
+            {
+                if (wb == null)
+                {
+                    wb = WorkBook.Load(zaposleni);
+                }
+                else
+                {
+                    Console.WriteLine("Some file has already been uploaded!");
+                }
+                return true;
+            }catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                wb = null;
+                return false;
+            }
+        }
+        public static bool CloseFile()
+        {
+            try{
+                if (wb != null)
+                {
+                    wb.Close();
+                    wb = null;
+                }
+                return true;
+            }catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                Console.WriteLine("Error closing");
+                return false;
+            }
+        }
+        public static bool LoadWorkSheet(int index)
+        {
+            try
+            {
+                if (wb != null)
+                {
+                    ws = wb.WorkSheets[index];
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                Console.WriteLine("Not loaded WorkSheet!");
+                ws = null;
+                return false;
+            }
+        }
+        public static string GetDataAt(int row, int column)
+        {
+            if (ws == null)
+            {
+                Console.WriteLine("WorkSheet not loaded!");
+                return "ERROR";
+            }
+            if (row < ws.Rows.Count)
+            {
+                RangeRow rangeRow = ws.Rows[row];
+                if (column < rangeRow.Columns.Count)
+                {
+                    RangeColumn rangeColumn = rangeRow.Columns[column];
+                    if (rangeColumn != null)
+                    {
+                        return rangeColumn.StringValue;
+                    }
+                    else
+                    {
+                        Console.WriteLine("There is no cell!");
+                        return "ERROR";
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("There is no column!");
+                    return "ERROR";
+                }
+            }
+            else
+            {
+                Console.WriteLine("There is no row!");
+                return "ERROR";
+            }
+        }
+        //Upisuje u excel tabelu podatak
+        public static bool SetData(int row, string data)
+        {
+            if (ws == null)
+            {
+                Console.WriteLine("Error setting data");
+                return false;
+            }
+
+            ws.SetCellValue(row, ws.Rows[row].Columns.Count, data);
+            wb.SaveAs(Constants.Excel_Path);
+            return true;
+        }
+        //Vraca broj redova
+        public static int GetRowCount()
+        {
+            if (ws == null)
+            {
+                Console.WriteLine("WorkSheet not loaded!");
+                return 0;
+            }
+            else
+            {
+                return ws.Rows.Count;
+            }
+        }
+
+
+
+
+
+
+    }
+}
